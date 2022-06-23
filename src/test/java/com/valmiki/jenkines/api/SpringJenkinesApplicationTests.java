b75a95a44bc07d0844f7bb7bacd9f54b02b8fc55
Arjun526 <nagarjuna.anke2@gmail.com>
2022-06-23 12:09:50 +0530

    no error now i guess

--- src/test/java/com/valmiki/jenkines/api/SpringJenkinesApplicationTests.java
+++ src/test/java/com/valmiki/jenkines/api/SpringJenkinesApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinesApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinesApplicationTests.class);
 
 	@Test
-	public  contextLoads() {
+	public void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
